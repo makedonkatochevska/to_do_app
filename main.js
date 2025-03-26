5a570fdda39d14f8c27b50530cb437b2f295b87e
Makedonka Tochevska <makedonkatochevska@gmail.com>
2025-03-26 12:42:33 +0100

    done

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 //html elements
 const form = document.getElementById("form");
 const ulList = document.getElementById("todoList");
+const allCheckedContainer = document.querySelector(".all-checked-container");
+const allCheckedInput = document.getElementById("allChecked");
 
 class ToDo {
-  constructor(_title, _id = null) {
+  constructor(_title, _id = null, _status = false) {
     this.title = _title;
-    this.status = false;
+    this.status = _status;
     this.id = _id || Date.now();
     this.isEditing = false;
   }
@@ -13,15 +15,33 @@ class ToDo {
 
 class ToDoLibrary {
   constructor() {
-    this.todos = [];
+    this.todos = this.retrieveListFromLocalStorage();
   }
 
   addTask(newTask) {
     this.todos.push(newTask);
+    this.saveListToLocalStorage(); //saves updated list
   }
 
   removeTask(todoID) {
     this.todos = this.todos.filter((todo) => todo.id !== todoID);
+    this.saveListToLocalStorage(); //saves updated list
+  }
+
+  saveListToLocalStorage() {
+    localStorage.setItem("taskList", JSON.stringify(this.todos));
+  }
+
+  retrieveListFromLocalStorage() {
+    const savedTodos = localStorage.getItem("taskList");
+
+    if (savedTodos) {
+      return JSON.parse(savedTodos).map(
+        (todo) => new ToDo(todo.title, todo.id, todo.status)
+      ); //return it with map() so they can be instance from Task class and not plain objects
+    } else {
+      return [];
+    }
   }
 }
 
@@ -39,6 +59,8 @@ function storeTasks() {
 
   form.todoInput.value = "";
 
+  updateAllCheckedCheckbox();
+
   console.log(toDoLibrary);
 }
 
@@ -48,6 +70,11 @@ function displayTasks() {
 
   toDoLibrary.todos.forEach((todo) => {
     const li = document.createElement("li");
+
+    //check status from storage and render it the same way
+    if (todo.status) {
+      li.classList.add("done");
+    }
 
     //Checkbox
     const checkbox = document.createElement("input");
@@ -60,6 +87,12 @@ function displayTasks() {
       todo.status = !todo.status;
       li.classList.toggle("done");
       editBtn.classList.toggle("button-disabled");
+
+      //save changes to local storage
+      toDoLibrary.saveListToLocalStorage();
+
+      //update if one is unchecked
+      updateAllCheckedCheckbox();
 
       console.log(toDoLibrary);
     });
@@ -84,6 +117,11 @@ function displayTasks() {
     editBtn.textContent = "Edit";
     editBtn.classList.add("button", "edit-button");
 
+    //check status from storage and render it the same way
+    if (todo.status) {
+      editBtn.classList.add("button-disabled");
+    }
+
     //edit btn event
     editBtn.addEventListener("click", () => {
       if (todo.isEditing) {
@@ -96,6 +134,9 @@ function displayTasks() {
           editInput.style.display = "none";
           label.style.display = "inline-block";
           editBtn.textContent = "Edit";
+
+          //save changes to local storage
+          toDoLibrary.saveListToLocalStorage();
         } else {
           alert("Please enter a valid task name!");
         }
@@ -105,16 +146,13 @@ function displayTasks() {
         editInput.style.display = "inline-block";
         label.style.display = "none";
         editBtn.textContent = "Save";
+
+        //save changes to local storage
+        toDoLibrary.saveListToLocalStorage();
       }
 
       console.log(toDoLibrary);
     });
-
-    //check status from storage and render it the same way
-    if (todo.status) {
-      li.classList.toggle("done");
-      editBtn.classList.toggle("button-disabled");
-    }
 
     //Delete btn
     const deleteBtn = document.createElement("button");
@@ -127,10 +165,17 @@ function displayTasks() {
       const confirmValue = confirm(
         "Are you sure you want to delete this ToDo item?"
       );
+
       if (!confirmValue) return;
 
-      toDoLibrary.removeTask(todo.id);
-      li.remove();
+      //falling effect
+      li.classList.add("falling");
+
+      setTimeout(() => {
+        toDoLibrary.removeTask(todo.id);
+        li.remove();
+        markAllAsCheckedDisplay();
+      }, 500);
 
       console.log(toDoLibrary);
     });
@@ -141,9 +186,66 @@ function displayTasks() {
   });
 }
 
+//mark all as checked fnc
+function markAllAsCheckedDisplay() {
+  allCheckedContainer.style.display =
+    toDoLibrary.todos.length <= 1 ? "none" : "flex";
+}
+
+function updateAllCheckedCheckbox() {
+  const liElements = document.querySelectorAll("li");
+
+  //check if all tasks are marked as done
+  const allDone =
+    [...liElements].length > 0 &&
+    [...liElements].every((element) => element.classList.contains("done"));
+
+  //update checkbox state
+  allCheckedInput.checked = allDone;
+}
+
+//mark all as checked event
+allCheckedInput.addEventListener("click", (e) => {
+  const liElements = document.querySelectorAll("li");
+  console.log(liElements);
+
+  //check if all tasks are already marked as done
+  const allDone = [...liElements].every((element) =>
+    element.classList.contains("done")
+  );
+
+  liElements.forEach((element, index) => {
+    const editBtn = element.querySelector(".edit-button");
+
+    if (allDone) {
+      //if all are done, uncheck all
+      element.classList.remove("done");
+      toDoLibrary.todos[index].status = false;
+      editBtn.classList.remove("button-disabled");
+    } else {
+      //otherwise, mark as done
+      element.classList.add("done");
+      toDoLibrary.todos[index].status = true;
+      editBtn.classList.add("button-disabled");
+    }
+  });
+
+  toDoLibrary.saveListToLocalStorage();
+});
+
 //form event
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   storeTasks();
   displayTasks();
+  markAllAsCheckedDisplay();
+
+  allCheckedInput.checked = false;
+});
+
+//load event
+window.addEventListener("DOMContentLoaded", () => {
+  displayTasks();
+  markAllAsCheckedDisplay();
+  updateAllCheckedCheckbox();
 });
